318bac9ecc7d58d37cc4ea840d465550d83f7fef
Janine Zielman <janinezielman@janines-mbp.kpn>
2023-04-25 10:26:19 +0200

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import { PrismicLink, PrismicRichText, PrismicText } from "@prismicio/react";
+import * as prismicH from "@prismicio/helpers";
+
+export const Footer = ({ settings }) => {
+  return (
+    <footer>
+      
+    </footer>
+  );
+};
