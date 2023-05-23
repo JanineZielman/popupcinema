7d4f3d39ff4cd80d8707aac867af08e96cef5816
Janine Zielman <janinezielman@janines-mbp.kpn>
2023-05-23 16:41:16 +0200

    background-generator

---
+++ pages/background.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from "react";
+
+const Background = () => {
+  function print(){
+    window.print();
+  }
+  
+  const [date, setDate] = useState('January');
+  const [time, setTime] = useState('20:00');
+
+  useEffect(() => {
+    document.getElementById("date").addEventListener("input", updateValue);
+    document.getElementById("time").addEventListener("input", updateValue);
+  })
+
+  function updateValue(){
+    setDate(document.getElementById("date").value);
+    setTime(document.getElementById("time").value);
+  }
+  
+  
+
+  return (
+    <div className="background-generator">
+      <div className="settings">
+        <div className="options">
+          <div>
+            Month:
+            <input className="date" id="date" placeholder="Janurary"></input>
+          </div>
+          <div>
+            Time:
+            <input className="time" id="time" placeholder="20:00"></input>
+          </div>
+        </div>
+        <div className="print" onClick={print}>
+          Print
+        </div>
+      </div>
+      <div className={`gradient ${date.slice(0, 3).toLowerCase()}1 ${date.slice(0, 3).toLowerCase()}2 time${time.slice(0, 2)}`}>
+        <div className="gradient-content">
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Background;
