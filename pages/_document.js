63aecb26107c7ca203ee57e992a64a9e34599f53
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-05-24 22:15:08 +0200

    font

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
         />
-        <link rel="stylesheet" href="https://use.typekit.net/txa4ybu.css"></link>
+       <link rel="stylesheet" href="https://use.typekit.net/txa4ybu.css"/>
       </Head>
       <body className="overflow-x-hidden antialiased">
         <Main />
