bfc909bf5929a2374c26e15e20b7d26d841a29e5
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-05-25 21:10:35 +0200

    logo

--- pages/logo.js
+++ pages/logo.js
@@ -6,19 +6,58 @@ import { PrismicNextImage } from "@prismicio/next";
 const Logo = ({ settings }) => {
   console.log(settings)
   return (
-    <div className="logo-animation">
+    <>
+    <div className="logo-animation" style={{ 
+      fontFamily: "new-hero",
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      transform: 'translate(-50%, -50%)',
+      textAlign: 'center',
+      }}>
       {prismicH.isFilled.image(settings.data.logo) && (
         <PrismicNextImage field={settings.data.logo} alt="logo"/>
       )}
-      <div className="loop">
+      <div className="loop" style={{ 
+          height: '95px',
+          overflow: 'hidden',
+        }}>
         {settings.data.slices[0].items.map((item, i) => {
           return(
-            <h1 key={`logo${i}`}>{item.text}</h1>
+            <h1 style={{ 
+              fontFamily: "new-hero",
+              fontWeight: '400',
+              fontSize: '54px',
+              whiteSpace: 'nowrap',
+              lineHeight: '1.4'
+            }} key={`logo${i}`}>{item.text}</h1>
           )
         })}
-        <h1>{settings.data.slices[0].items[0].text}</h1>
+        <h1 style={{ 
+          fontFamily: "new-hero",
+        }}>
+        {settings.data.slices[0].items[0].text}</h1>
       </div>
+      <style jsx global>{`
+      .logo-animation{
+        .loop h1:first-child{
+          margin-top: 0;
+          animation: logoAni 6s ease 1s infinite;
+          // animation-fill-mode: forwards;
+        }
+      }
+
+      @keyframes logoAni {
+        0%   {margin-top: 0px;}
+        20%  {margin-top: -100px;}
+        40%  {margin-top: -200px;}
+        60%  {margin-top: -300px;}
+        80%  {margin-top: -400px;}
+        100%  {margin-top: -500px;}
+      }
+    `}</style>
     </div>
+    </>
   );
 };
 
