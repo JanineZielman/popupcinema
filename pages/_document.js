41fbc2e35e2c3345f46dbdf4cddf85161d931b23
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-06-17 14:26:18 +0200

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,16 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
-        />
-       <link rel="stylesheet" href="https://use.typekit.net/txa4ybu.css"/>
-       <script async src="https://eocampaign1.com/form/e621049e-0aba-11ee-a2d7-fb653a89d3ac.js" data-form="e621049e-0aba-11ee-a2d7-fb653a89d3ac"></script>
+        />   
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ededed"/>
+        <meta name="msapplication-TileColor" content="#ededed"/>
+        <meta name="theme-color" content="#ffffff"></meta>
+        <link rel="stylesheet" href="https://use.typekit.net/txa4ybu.css"/>
+        <script async src="https://eocampaign1.com/form/e621049e-0aba-11ee-a2d7-fb653a89d3ac.js" data-form="e621049e-0aba-11ee-a2d7-fb653a89d3ac"></script>
       </Head>
       <body className="overflow-x-hidden antialiased">
         <Main />
